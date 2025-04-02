544fbe8c5e657f8b20282941aa761101eba2e7a7
TriHuynh <huynhminhtri1109@gmail.com>
2025-04-02 15:57:28 +0700

    [add] finally success when add notification realtime

--- RiskChance/wwwroot/js/Notification.js
+++ RiskChance/wwwroot/js/Notification.js
@@ -11,37 +11,48 @@
         .withUrl("/notificationHub")
         .build();
     connection.on("ReceiveNotification", function (message) {
-        addNotify(message);
+        console.log(typeof (message));
+        console.log("Received message:", message);
+
+        // Tạo một thẻ div mới
+        var notificationDiv = document.createElement("div");
+        notificationDiv.classList.add("notification-item");
+
+        // Gán nội dung vào div
+        notificationDiv.innerHTML = `
+            <a id="wrapper-${message.idNoti}" href="#" onclick="markAsRead(${message.idNoti})" 
+               class="container-fluid notify-box d-flex align-items-center text-decoration-none">
+                <div class="avatar-box d-none d-sm-block">
+                    <img class="rounded-circle user-img"
+                         src="${message.nguoiGui && message.nguoiGui.AvatarUrl ? message.nguoiGui.AvatarUrl : '/assets/user/image.png'}"
+                         alt="avatar" />
+                </div>
+                <div class="notify-content">
+                    <p class="line-clamp-2">${message.noiDung}</p>
+                    <span class="date-text">${new Date(message.ngayGui).toLocaleDateString('en-GB')}</span>
+                </div>
+                <div class="notify-status d-none d-sm-block">
+                    <div id="status-${message.idNoti}" class="status rounded-circle"></div>
+                </div>
+            </a>
+        `;
+
+        // Lấy container và thêm notification vào đầu danh sách
+        var container = document.getElementById("notificationListContainer");
+        if (container) {
+            container.insertBefore(notificationDiv, container.firstChild);
+            console.log("Notification added successfully!");
+        } else {
+            console.error("Container #notificationListContainer not found!");
+        }
     });
+
 
     // Bắt đầu kết nối SignalR
     connection.start().catch(function (err) {
         return console.error(err.toString());
     });
 });
-
-function addNotify(message) {
-    var mess = `<a id="wrapper-${message.IDNoti}" href="#" onclick="markAsRead(${message.IDNoti})" class="container-fluid notify-box d-flex align-items-center text-decoration-none">
-            <div class="avatar-box d-none d-sm-block">
-                <img class="rounded-circle user-img"
-                     src="${message.NguoiGui.AvatarUrl ? message.NguoiGui.AvatarUrl : '/assets/user/image.png'}"
-                     alt="avatar" />
-            </div>
-
-            <div class="notify-content">
-                <p class="line-clamp-2">
-                    ${message.NoiDung}
-                </p>
-                <span class="date-text">${new Date(message.NgayGui).toLocaleDateString('en-GB')}</span>
-            </div>
-
-            <div class="notify-status d-none d-sm-block">
-                <div id="status-${message.IDNoti}" class="status rounded-circle"></div>
-            </div>
-        </a>`;
-
-    $('#notificationListContainer ').prepend('<div class="notification-item">' + mess + '</div>');
-}
 
 // Đánh dấu thông báo là đã đọc
 function markAsRead(notificationId) {
